6c63429a7b8e2fd4f52f4ecf859feaacaf9a340e
Bulent Karaahmed <bulent.karaahmed@progress.com>
2018-07-12 14:32:33 +0300

    added fat library target with build script

---
+++ SocketRocket_progress/SocketRocket_progress.h
@@ -0,0 +1,18 @@
+//
+//  SocketRocket_progress.h
+//  SocketRocket_progress
+//
+//  Created by Bulent Karaahmed on 29.06.18.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SocketRocket_progress.
+FOUNDATION_EXPORT double SocketRocket_progressVersionNumber;
+
+//! Project version string for SocketRocket_progress.
+FOUNDATION_EXPORT const unsigned char SocketRocket_progressVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SocketRocket_progress/PublicHeader.h>
+
+
